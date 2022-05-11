c42f720478d91f02cb66709f9ae51df6b6ce06c4
olsi10 <wangbampang2035@gmail.com>
2022-05-11 23:38:22 +0900

    const 상수, integer a.k.a full number 정수

--- vanillaJS/app.js
+++ vanillaJS/app.js
@@ -1,1 +1,19 @@
-alert("hello");
+//alert("hello");
+
+//integer, full number(정수)
+
+console.log(6 + 1);
+console.log(6 * 1);
+console.log(6 / 1);
+
+//값을 변경하고 싶다면? use variable(값을 저장하거나 유지)
+
+//(상수)
+const a = 5;
+const b = 2;
+const myName = "nico";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
