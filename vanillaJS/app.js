52345222fafebe975618d458ec75a06585fc3868
olsi10 <wangbampang2035@gmail.com>
2022-05-12 15:02:32 +0900

    함수 개념 및 실습

--- vanillaJS/app.js
+++ vanillaJS/app.js
@@ -74,3 +74,74 @@ const toBuy = ["potato", "tomato", "pizza"];
 toBuy.push("kimbab");
 console.log(toBuy);
 console.log(toBuy[2]);
+
+// const playerName = "nico";
+// const playerPoints = 121212;
+// const playerHandsome = false;
+// const playerFat = "little bit";
+
+// player.name
+// player.points
+
+//const player = ["nico", 1212, false, "little bit"];
+//배열로 정의하면 이런 식.
+
+//그래도 일일이 정의하기 귀찮으니 '객체' 만들기!!
+
+//Object
+
+const player = {
+    name : "nico",
+    points : 10,
+    handsome : false,
+    fat : true
+};
+
+console.log(player); //객체 호출
+console.log(player.name); //객체 요소 호출
+console.log(player["name"]); //또 다르게 요소에 접근하는 방법
+
+player.fat = false; //요소의 값 변경
+//player는 const로 정의되어 있지만 const 안의 요소들은 각자의 데이터 타입,
+//즉, 변경 가능한 변수이기 때문에 변경 가능
+console.log(player);
+
+player.lastName = "potato"; //요소의 값 추가
+console.log(player);
+
+
+///////////////////////////////////
+
+//player.points() <- ()는 function을 의미
+
+//function은 내가 계속하여 쓸 수 있는 코드 조각
+
+function sayHi() {
+    console.log("hello");
+} // {} 코드 블록으로 감쌈
+
+sayHi(); //함수 호출
+
+
+// alert(); //empty alert
+// console.log(); //콘솔창에 아무것도 없음
+
+//sayHi("hello"); //sending data
+
+//hello -> Hi! my name is [ ] 로 호출되도록 변경
+
+function sayHi(nameOfPerson, age) { //인자(매개변수) 값
+    console.log("hi, " + nameOfPerson + " your age is " + age + ".");
+}
+
+sayHi("olsi", 18); // olsi, 18 // 매개변수가 두 개이므로 두 개 작성
+
+////////////////////////////////////////////
+
+function plus(a, b) {
+    console.log(a + b);
+}
+
+plus() //undefined, undefined / NaN (Not a Number)
+plus(3, 5); //3 ,5
+
