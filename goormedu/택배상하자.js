6bdcd66980d3b0d06f341262d3b731746fec0696
olsi10 <wangbampang2035@gmail.com>
2022-06-06 02:08:49 +0900

    택배 상하자 Stack 구조 (LIFO)

---
+++ goormedu/택배상하자.js
@@ -0,0 +1,20 @@
+let del = []
+
+del.push("아이패드");
+del.push("햇반");
+del.push("생수");
+del.push("피아노");
+
+//del = ["아이패드", "햇반", "생수", "피아노"]
+
+del.pop(); //배열의 마지막 원소를 "펑!!"
+
+//del = ["아이패드", "햇반", "생수"]
+
+del.pop(); //배열의 마지막 원소를 "펑!!"
+
+//del = ["아이패드", "햇반"]
+
+let dell = del.pop() //햇반을 갖고 있는 dell 변수
+
+console.log(dell)
