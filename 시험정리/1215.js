c604936c713441e3138688cab75063c679a9325f
olsi10 <wangbampang2035@gmail.com>
2022-12-15 15:11:27 +0900

    메소드 바인딩

--- 시험정리/1215.js
+++ 시험정리/1215.js
@@ -41,3 +41,56 @@
 
 // // prototype에 추가하고 싶다면
 // User.prototype.name = "John";
+
+///////////////////////////////////////////////////
+// 클래스 필드로 바인딩 된 메서드 만들기
+// 자바스크립트는 함수 호출에 따른 this값을 가진다.
+// 함수뿐만이 아닌 메서드에도 적용
+// 메서드를 전달하여 다른 컨텍스트에서 호출하면 this값은 원래 객체 참조 x
+
+// class Button {
+//     constructor(value) {
+//         this.value = value;
+//     }
+//     click() {
+//         alert(this.value);
+//     }
+// }
+// let button = new Button("hello");
+// // 메서드 전달 (단, 메서드가 전달되어 콜백 함수에서 호출되므로 함수가 호출되는 시점에는
+// // this 맥락(점 앞 객체)을 잃게 됨)
+// setTimeout(button.click, 1000); // undefined
+
+//////////////////////////////////////////////
+// 그럴 때엔 화살표 함수를 사용한다
+// 화살표 함수를 쓰면 객체마다 독립적인 함수를 만들고
+// 함수의 this를 해당 객체에 바인딩
+// 따라서 메서드가 실행되는 맥락에 상관없이 this에 항상 의도한 값이 들어감
+// 메서드를 이벤트 리스너로 해야할 때 유용
+
+// class Button {
+//     constructor(value) {
+//     this.value = value;
+//     }
+//     // click 메서드를 화살표 함수를 이용해서 정의
+//     click = () => {
+//     alert(this.value);
+//     }
+//     nonBinded() {
+//     alert(this.value);
+//     }
+//     }
+//     let button1 = new Button("hello");
+//     setTimeout(button1.click, 1000); // hello
+//     let button2 = new Button("hello");
+//     // click 메서드는 화살표 함수로 정의했으므로,
+//     // 서로 독립적인 함수 객체 참조를 가지게 됨 (내용은 똑같지만 서로 참조가 다른 독립적인 함수)
+//     console.log( button1.click === button2.click ); // false
+//     // nonBinded 메서드는 화살표 함수를 이용하지 않고 정의했으므로,
+//     // 화살표 함수로 정의하지 않았으므로 함수 객체를 공유
+//     console.log( button1.nonBinded === button2.nonBinded ); // true
+//     // click은 prototype 객체에도 포함되지 않음
+//     console.log( Button.prototype.click );
+//     // 그러나 nonBinded는 prototype 객체에 포함됨
+//     console.log( Button.prototype.nonBinded )
+
