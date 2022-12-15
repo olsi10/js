ed74b17bf6339b28f241f97856d921bdace02d87
olsi10 <wangbampang2035@gmail.com>
2022-12-15 12:10:09 +0900

    클래스와 프로토타입

---
+++ 시험정리/1215.js
@@ -0,0 +1,25 @@
+// // User라는 함수를 만들고 함수 본문은 생성자 메서드에서 가져옴
+// class User {
+//     // 생성자 메서드가 없으면 본문이 비워진 상태로 함수 생성
+//     constructor(name) { this.name = name; }
+//     // sayHi와 같은 클래스 내에서 정의한 메서드를 함수의 prototype 객체에 추가
+//     sayHi() { alert(this.name); }
+// }
+
+// console.log(typeof User); // function
+
+// console.log(User.prototype) // constructor : User, sayHi : f
+
+// 1. User 라는 이름을 가진 생성자 함수를 만들고 함수 본문은 생성자 메서드 constructor에서 가져오기
+function User(name) {
+    this.name = name;
+}
+
+// 만약 construcctor에 메서드가 없다면 본문이 비워진 생성자 함수 정의
+// function User(name) {}
+
+// 2. 클래스 내에서 사용할 메서드를 User.prototype에 정의하기
+User.prototype.sayHi = function() { alert(this.name); }
+
+let u = new User("John");
+u.sayHi()
